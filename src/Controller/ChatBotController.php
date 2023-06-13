7b9266c60087a696deb25f1836334000d69fef2b
JeremieN6 <ngoyi.jeremie@gmail.com>
2023-06-13 10:19:42 +0200

    Link with Open Ai API + render response in Twig

--- src/Controller/ChatBotController.php
+++ src/Controller/ChatBotController.php
@@ -2,24 +2,38 @@
 
 namespace App\Controller;
 
+use App\Form\QuestionsType;
+use App\Service\OpenAiService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Service\OpenAIChatService;
 use Symfony\Component\HttpFoundation\Request;
 
 class ChatBotController extends AbstractController
 {
     #[Route('/chat-bot', name: 'app_chat_bot')]
-    public function chatBot(Request $request, OpenAIChatService $openAIChatService): Response
+    public function chatBot(Request $request, OpenAiService $openAi): Response
     {
 
-        $message = $request->get('message');
-        $response = $openAIChatService->sendMessage($message);
+
+        $form = $this->createForm(QuestionsType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $data = $form->getData();
+            $json = $openAi->getDestination($data['destination']);
+            return $this->render('chat_bot/chatBot.html.twig', [
+                'form' => $form->createView(),
+                'json' => $json ?? null,
+            ]);
+        }   
+        // $message = $request->get('message');
+        // $response = $openAIChatService->sendMessage($message);
         
         return $this->render('chat_bot/chatBot.html.twig', [
-            'response' => $response,
+            'form' => $form->createView(),
             'controller_name' => 'HomeController',
+            'json' => $json ?? null,
         ]);
     }
 }
---
+++ src/Service/OpenAiService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Orhanerday\OpenAi\OpenAi;
+use Symfony\Component\Serializer\Encoder\JsonDecode;
+
+class OpenAiService{
+    public function __construct(
+        private ParameterBagInterface $parameterBag,
+    )
+    {
+        
+    }
+    public function getDestination(string $destination): string
+    {
+        
+     
+        $openai_api_key = $this->parameterBag->get('OPENAI_API_KEY');
+        $open_ai = new OpenAi($openai_api_key);
+
+        $complete = $open_ai->completion([
+            'model' => 'text-davinci-003',
+            'prompt' => 'En tant qu\'expert Guide touristique, trouve moi 5 choses à faire en me faisant une liste. Reviens à la ligne et saute une ligne à chaque nouveau point. Donne un maximum de détail en citant la ville(s), le(s) restaurant(s), la plage(s). Pour un voyage de 2 semaines à'.$destination,
+            // 'prompt' => 'Trouve moi des choses à voir pour mon voyage à {{destination}}. Pour ce voyage il y a {nbr_personne} pour {nbr_jour} jours. On a une aversion pour {aversion}.', Ajoute les variables dans les parametres de la fonction sendMessage
+            'temperature' => 0,
+            'max_tokens' => 3500,
+            'frequency_penalty' => 0.5,
+            'presence_penalty' => 0,
+        ]);
+
+        $json = json_decode($complete, true);
+
+        if (isset($json['choices'][0]['text'])) {
+            $json = $json['choices'][0]['text'];
+
+            return $json;
+        }
+
+        $json = 'Une erreur est survenue !';
+
+        return $json;
+    }
+}
