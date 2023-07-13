e078f262bd9abe8b32f10c1f600663115053887b
JeremieN6 <ngoyi.jeremie@gmail.com>
2023-07-13 14:39:39 +0200

    Form renseignement front + reception data back

--- src/Form/QuestionsType.php
+++ src/Form/QuestionsType.php
@@ -2,11 +2,13 @@
 
 namespace App\Form;
 
-use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CurrencyType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType as TypeTextType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,13 +17,54 @@ class QuestionsType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('destination', TypeTextType::class, [
-                'label' => 'Votre destination',
+            ->add('destination', TextType::class, [
+                'label' => 'Destination',
                 'attr' => [
                     'placeholder' => 'Entrez la ville de destination'
                 ],
             ])
-
+            ->add('duree_sejour', IntegerType::class, [
+                'label' => 'Durée du séjour',
+                'attr' => [
+                    'placeholder' => 'Entrez la durée de votre séjour (en jours)'
+                ],
+            ])
+            ->add('nombre_personne_sejour', IntegerType::class, [
+                'label' => 'Nombre de personne(s)',
+                'attr' => [
+                    'placeholder' => 'Entrez le nombre de personne(s) pour ce séjour'
+                ],
+            ])
+            ->add('budget_sejour', IntegerType::class, [
+                'label' => 'Votre budget',
+                'attr' => [
+                    'placeholder' => 'Entrez votre budget pour ce séjour (Budget par personne si vous voyagez en groupe)'
+                ],
+            ])
+            ->add('saison_destination', ChoiceType::class, [
+                'label' => 'Saison de destination',
+                'choices' => [
+                    'Été' => "Ete",
+                    'Automne' => "Automne",
+                    'Hiver' => "Hiver",
+                    'Printemps' => "Printemps"
+                ],
+                'attr' => [
+                    'placeholder' => 'Entrez la saison qu\'il fera lors de votre départ',
+                ],
+            ])
+            ->add('interet_preference', TextareaType::class, [
+                'label' => 'Vos intérêts & préférence',
+                'attr' => [
+                    'placeholder' => 'Saisissez en un mot ce que vous aimez faire durant vos vacances. (Mettez une virgule après chaque mot)'
+                ],
+            ])
+            ->add('restrictions', TextareaType::class, [
+                'label' => 'Vos restictions & avertions',
+                'attr' => [
+                    'placeholder' => 'Saisissez en un mot ce qu\'il y a à savoir sur vous concernant vos alergies alimentaires, vos aversions lors de vos voyages'
+                ],
+            ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Valider',
                 ])
--- src/Service/OpenAiService.php
+++ src/Service/OpenAiService.php
@@ -13,7 +13,14 @@ class OpenAiService{
     {
         
     }
-    public function getDestination(string $destination): string
+    public function getDestination(
+        string $destination, 
+        int $duree_sejour, 
+        int $nombre_personne_sejour,
+        int $budget_sejour,
+        string $saison_destination,
+        array $interet_preference,
+        array $restrictions): string
     {
         
      
@@ -22,7 +29,14 @@ class OpenAiService{
 
         $complete = $open_ai->completion([
             'model' => 'text-davinci-003',
-            'prompt' => 'En tant qu\'expert Guide touristique, fais moi un planning sur mes 2 semaines de vacances en me trouvant des choses à faire en me faisant une liste. Reviens à la ligne et saute une ligne à chaque nouveau jour. Donne un maximum de détail en citant la ville(s), le(s) restaurant(s), la plage(s). Pour un voyage de 2 semaines à'.$destination,
+            'prompt' => 'En tant qu\'expert Guide touristique.
+            Fais moi un planning sur mon séjour dont la destination est :' .$destination,' et qui vas durer'.$duree_sejour,'jours.
+            Le nombre de voyageur pour ce séjour est de'.$nombre_personne_sejour,'personnes.
+            Le budget par personne est de'.$budget_sejour,'.
+            Pendant mon voyage la saison de la destination sera en : '.$saison_destination,'.
+            Je vais pour finir te donner une liste de mes préférence lorsque je voyage : '.$interet_preference,'.
+            et une liste de toute les choses dont je ne veux pas entendre parler pendant mon séjour : '.$restrictions,'
+            En te basant sur les élements que je t\'ai fourni, rédige moi une liste en revenant à la ligne pour chaque nouveau jour.',
             // 'prompt' => 'Trouve moi des choses à voir pour mon voyage à {{destination}}. Pour ce voyage il y a {nbr_personne} pour {nbr_jour} jours. On a une aversion pour {aversion}.', Ajoute les variables dans les parametres de la fonction sendMessage
             'temperature' => 0,
             'max_tokens' => 3500,
